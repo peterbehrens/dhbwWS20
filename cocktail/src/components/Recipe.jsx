b7a713667f4d25a166964bf37ea6be9049159dab
Behrens <peter.behrens@hpe.com>
2020-02-26 14:16:30 +0100

    Matching available ingredients with possible recipes and added a shopping boolean

--- cocktail/src/components/Recipe.jsx
+++ cocktail/src/components/Recipe.jsx
@@ -1,17 +1,23 @@
-import React, { Component } from 'react'
-import axios from "axios"
+import React, { Component } from 'react';
+import axios from "axios";
+import prepare from "./Preparation";
+import Grid from '@material-ui/core/Grid';
 
 export default class all_recipes extends Component {
 
     constructor(props){
         super(props);
         this.state ={
-            recipes : []
+            download : [],
+            recipes : [],
+            shopping : true
         }
     }
 
     componentDidMount(){
         this.getDatabaseUpdate()
+        this.state.shopping = this.props.shopping
+        console.log("Domponent did mount")
     }
 
     getDatabaseUpdate(){
@@ -23,17 +29,82 @@ export default class all_recipes extends Component {
                     ...response.data[key], 
                     id : key
                 })
-            this.state.recipes = fetchedOrders
+            this.state.download = fetchedOrders
             }
-            this.forceUpdate()
+            this.setRecipes()
     })}
 
+    setRecipes(){
+        this.state.download.map(recipe => {
+            const ingredients = recipe.ingredients.split(",")
+            const recipename = recipe.name
+            var newelement = {}
+            newelement["ingredients"] = ingredients
+            newelement["name"] = recipename
+            this.state.recipes.push(newelement)
+        })
+        this.checkMatchingRateIngRec()
+    }
+
+    checkMatchingRateIngRec(){
+        this.state.recipes.forEach( recipe => {
+            var ret = []
+            var matchingRate = 0
+            for(var i in recipe) {
+                for(var ingredients in recipe[i]){
+                    if(this.props.ingredients.indexOf(recipe[i][ingredients]) > -1){
+                        ret.push(recipe[i][ingredients]);
+                    }
+                }
+            if (i === "ingredients"){
+            matchingRate = ret.length / recipe[i].length
+            recipe.matchingRate = matchingRate
+            }
+            }
+        })
+        this.forceUpdate()
+        this.state.recipes.sort(this.compareValues("matchingRate"))
+        this.forceUpdate()
+    }
+
+    compareValues(key, order = 'desc') {
+        return function innerSort(a, b) {
+            if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            return 0;
+            }
+        
+            const varA = (typeof a[key] === 'string')
+            ? a[key].toUpperCase() : a[key];
+            const varB = (typeof b[key] === 'string')
+            ? b[key].toUpperCase() : b[key];
+        
+            let comparison = 0;
+            if (varA > varB) {
+            comparison = 1;
+            } else if (varA < varB) {
+            comparison = -1;
+            }
+            return (
+            (order === 'desc') ? (comparison * -1) : comparison
+            );
+        };
+    }
+
     render() {
-        console.log(this.state.recipes)
-        return (
-            <div>
-                
-            </div>
+        return(
+        this.state.recipes.map(item => {
+            return (
+                <Grid container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                >
+                    <Grid>
+                        <Rezept key={item.name} shopping={this.state.shopping} recipe = {item}/>
+                    </Grid>
+                </Grid>
+            )   
+        })
         )
     }
 }
